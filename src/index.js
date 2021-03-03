abbe5667fb771b6bf23797fc8e32e1c43478bcc5
kirillank <kirill.ank04@yandex.by>
2021-03-03 18:59:34 +0300

    fix: fix code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 
 exports.min = function min (array) {
-  return 0;
+  let result = Math.min.apply(null,array);
+  if(array&&array.length!==0) {
+    return result;
+  } else {
+    return 0;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  let resultmax = Math.max.apply(null,array);
+  if(array&&array.length!==0) {
+    return resultmax;
+  } else {
+    return 0;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let sum = array.reduce((a, b) => a + b); 
+  let avg = sum /array.length;
+  if (array&&array.length!==0){
+      return avg;
+  } else {
+    return 0;
+  }
 }
